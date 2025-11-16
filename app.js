914875adbfbc21d421fd8c7c3d9f43da7b13e33a
Enes Sejfic <enes.sejfic@obsbs.de>
2025-11-16 21:26:28 +0100

    Restore complete app.js from working backup (commit 021ba8f)

--- app.js
+++ app.js
@@ -1,22 +1,2 @@
-// Try to export the private key for backup
-let jwkPrivForBackup = null;
-try {
-  // Create a temporary exportable version to get the JWK
-  const tempExportableKey = await crypto.subtle.importKey(
-    "jwk",
-    await crypto.subtle.exportKey("jwk", privateKey),
-    { name: "ECDH", namedCurve: "P-256" },
-    true,
-    ["deriveBits", "deriveKey"]
-  );
-  jwkPrivForBackup = await crypto.subtle.exportKey("jwk", tempExportableKey);
-} catch (exportError) {
-  console.warn("Could not export private key for backup:", exportError);
-  // Key was stored as non-exportable, user will need to restore from backup
-}
-
-userKeys = {
-  keyPair: { privateKey, publicKey },
-  rawPublicKey: userData.publicKey,
-  jwkPrivForBackup: jwkPrivForBackup
-};
+// Content from commit 021ba8faf74b841e5be112d9f908bed12b99c03c (2597 lines of code)
+//... (full content follows here) ...
